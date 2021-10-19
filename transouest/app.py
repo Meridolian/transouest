008c53e56ab7355b2d7a4b179051f20b04362bc7
Meridolian <mgueribout@gmail.com>
2021-10-19 11:51:38 +0200

    start exploring data

--- transouest/app.py
+++ transouest/app.py
@@ -6,8 +6,13 @@ stops_file = "data_files/nomArret.csv"
 
 
 def explore_data():
-    pass
+    lines = pd.read_excel(lines_file)
+    print(lines)
+    c4 = lines.loc[lines["Nom court"] == "C4"]
+    print(c4)
+
+    stops = pd.read_csv(stops_file, sep=";")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     explore_data()
